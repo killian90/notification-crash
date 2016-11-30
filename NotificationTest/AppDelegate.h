bbac31dcaad1ef97be8a283cd4a97f3fd743d7fa
Killian O'Connell <killian.oconnell@bighealth.com>
2016-11-30 16:52:47 +0000

    Initial Commit

---
+++ NotificationTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NotificationTest
+//
+//  Created by Killian O'Connell on 30/11/2016.
+//  Copyright © 2016 BIG HEALTH LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NotificationTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NotificationTest
+//
+//  Created by Killian O'Connell on 30/11/2016.
+//  Copyright © 2016 BIG HEALTH LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
